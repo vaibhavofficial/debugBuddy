61b4dbabec26fb207aa4de233df17f99fbc19329
Vaibhav Saware <vaibhavsaware.vs@gmail.com>
2024-07-25 18:25:24 +0530

    tree node add

---
+++ src/app/shared/components/tree-node/tree-node.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-node',
+  templateUrl: './tree-node.component.html',
+  styleUrl: './tree-node.component.scss'
+})
+export class TreeNodeComponent {
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { MaterialModule } from './material.module';
 import { NodeListComponent } from './components/node-list/node-list.component';
 import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { ListComponent } from './components/list/list.component';
+import { TreeNodeComponent } from './components/tree-node/tree-node.component';
 
 @NgModule({
   imports: [MaterialModule],
-  declarations: [NodeListComponent, ProgressBarComponent, ListComponent],
+  declarations: [NodeListComponent, ProgressBarComponent, ListComponent, TreeNodeComponent],
   exports: [
     MaterialModule,
     NodeListComponent,
